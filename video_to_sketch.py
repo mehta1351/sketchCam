9d1cdad48481d73883503c5b17e7169b77e079b6
Pranav Mehta <75165596+mehta1351@users.noreply.github.com>
2021-12-18 14:54:21 +0530

    Update video_to_sketch.py

--- video_to_sketch.py
+++ video_to_sketch.py
@@ -2,18 +2,18 @@ import cv2
 import numpy as np 
 def sketch(image):
     img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    img_gray_blur = cv2.GaussianBlur(img_gray,(5,5),0)
-    canny_edges = cv2.Canny(img_gray_blur,10,70)
-    ret,mask = cv2.threshold(canny_edges,70,255,cv2.THRESH_BINARY_INV)
+    img_gray_blur = cv2.GaussianBlur(img_gray,(4,5),0)
+    canny_edges = cv2.Canny(img_gray_blur,10,60)
+    ret,mask = cv2.threshold(canny_edges,60,240,cv2.THRESH_BINARY_INV)
     return mask 
 
-cap = cv2.VideoCapture(0)# for live if want to use a precorded video , enter path of video in place of "0"
+cam = cv2.VideoCapture(0)# for live if want to use a precorded video , enter path of video in place of "0"
 
 while(True):
-    ret,frame = cap.read()
+    ret,frame = cam.read()
     cv2.imshow("Live Sketch 😁 " , sketch(frame))
     if(cv2.waitKey(1) == 13):
         break
 
-cap.release()
+cam.release()
 cv2.destroyAllWindows()    
